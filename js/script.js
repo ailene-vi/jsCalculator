6e38f1979ebd130eb118375667acf6db1dcf138e
ailene-vi <ailenevilla3@gmail.com>
2026-06-30 18:13:23 -1200

    added 3rd number

--- js/script.js
+++ js/script.js
@@ -1,13 +1,38 @@
-function calculateSum(){
+function calculateSum() {
     const num1 = document.getElementById('num1').value;
     const num2 = document.getElementById('num2').value;
+    const num3 = document.getElementById('num3').value;
 
     const number1 = parseFloat(num1);
     const number2 = parseFloat(num2);
+    const number3 = parseFloat(num3);
 
-    //Calculate Sum
-    const sum = number1 + number2;
-    
-    //display the result
-    document.getElementById('result').textContent='The sum is ' +sum;
+    // Calculate Sum
+    const sum = number1 + number2 + number3;
+
+    // Display the result
+    document.getElementById('result').textContent = 'The sum is ' + sum;
+}
+
+function calculateMultiply() {
+    const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
+    const num3 = document.getElementById('num3').value;
+
+    const number1 = parseFloat(num1);
+    const number2 = parseFloat(num2);
+    const number3 = parseFloat(num3);
+
+    // Calculate Product
+    const product = number1 * number2 * number3;
+
+    // Display the result
+    document.getElementById('result').textContent = 'The product is ' + product;
+}
+
+function clearInputs() {
+    document.getElementById('num1').value = "";
+    document.getElementById('num2').value = "";
+    document.getElementById('num3').value = "";
+    document.getElementById('result').textContent = "This is for the answer";
 }
